1251a3fdf7337d018aee0cec0f456d04cc0864f3
Yan Lukashin <93304525+YanLukashin@users.noreply.github.com>
2021-11-06 11:35:25 +0700

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -3,9 +3,9 @@
 
 pragma solidity ^0.8.0;
 
-import "https://github.com/YanLukashin/recovery-token/blob/main/IERC20.sol";
-import "https://github.com/YanLukashin/recovery-token/blob/main/IERC20Metadata.sol";
-import "https://github.com/YanLukashin/recovery-token/blob/main/Context.sol";
+import "@YanLukashin/recovery-token/blob/main/IERC20.sol";
+import "@YanLukashin/recovery-token/blob/main/IERC20Metadata.sol";
+import "@YanLukashin/recovery-token/blob/main/Context.sol";
 
 /**
  * @dev Implementation of the {IERC20} interface.
