debd2cc2e2b8acd72c4e8fd014ff23075d265743
Yan Lukashin <93304525+YanLukashin@users.noreply.github.com>
2021-11-06 11:24:36 +0700

    Update TokenRecover.sol

--- TokenRecover.sol
+++ TokenRecover.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "https://github.com/YanLukashin/recovery-token/blob/main/Ownable.sol";
 
 /**
  * @title TokenRecover
