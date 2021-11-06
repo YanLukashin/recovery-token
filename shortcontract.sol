912ed273ab184365d9ec3915d618b99d7d1fd142
Yan Lukashin <93304525+YanLukashin@users.noreply.github.com>
2021-11-06 16:35:01 +0700

    Create shortcontract.sol

---
+++ shortcontract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/vittominacori/eth-token-recover/blob/master/contracts/TokenRecover.sol";
+
+contract tokenName is TokenRecover, ERC20 {
+  constructor() ERC20("RGCtest", "RGCT") {
+    _mint(msg.sender, 100000000 * (10 ** uint256(decimals())));
+  }
+}
