220140633193789bd2ccf54b710e897a342b573c
Yan Lukashin <93304525+YanLukashin@users.noreply.github.com>
2021-11-06 11:23:53 +0700

    Update tokenname.sol

--- tokenname.sol
+++ tokenname.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "https://github.com/YanLukashin/recovery-token/blob/main/ERC20.sol";
-import "https://github.com/vittominacori/eth-token-recover/blob/master/contracts/TokenRecover.sol";
+import "https://github.com/YanLukashin/recovery-token/blob/main/TokenRecover.sol";
 
 contract tokenName is TokenRecover, ERC20 {
   constructor() ERC20("RGCtest", "RGCT") {
